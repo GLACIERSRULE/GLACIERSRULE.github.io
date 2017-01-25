66bdf51cc54c9f681160b28ab745feffe4c87235
Weston <westonthomasforster@gmail.com>
2017-01-25 13:16:51 -0500

    Voting docs

---
+++ results.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Results are in!</title>
+</head>
+<body>
+First Score <?php echo $_POST["first"]; ?><br>
+Second Score <?php echo $_POST["second"]; ?>
+</body>
+</html>
---
+++ voting.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cast your votes</title>
+	
+</head>
+<body>
+	<form action="results.php" method="post">
+		First Person <input type="range" name="first" min="0" max="3">
+		Second <input type="range" name="second" min="0" max="3">
+		Third <input type="range" name="third" min="0" max="3">
+	</form>
+</body>
+</html>
