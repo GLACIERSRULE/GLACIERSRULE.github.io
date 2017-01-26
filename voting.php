5a5bcb2406a0eb2d6b4243e8661647aa5be84a3e
Weston <westonthomasforster@gmail.com>
2017-01-25 20:18:22 -0500

    more voting

---
+++ voting.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cast your votes</title>
+	
+</head>
+<body>
+	<form action="results.php" method="post">
+		First Person <input type="range" name="first" min="0" max="3">
+		Second <input type="range" name="second" min="0" max="3">
+		Third <input type="range" name="third" min="0" max="3">
+		Submit <input type="Submit" name="Submit">
+	</form>
+</body>
+</html>
